f90701c27fcf1392f78571ac68ae40aac880a9b3
jjyepez <jjyepez@gmail.com>
2018-06-28 02:02:01 -0400

    Avances - Clase 10

--- App.js
+++ App.js
@@ -5,11 +5,11 @@
  */
 
 import React, { Component } from 'react';
+
+import Home from './src/views/containers/HomeC'
+
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+  Text
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -23,36 +23,12 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native 2x1!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Home>
+        <Text>header</Text>
+        <Text>buscador</Text>
+        <Text>categorias</Text>
+        <Text>sugerencias</Text>
+      </Home>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/views/containers/HomeC.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class HomeC extends Component {
+    render(){
+        return this.props.children
+    }
+}
+
+export default HomeC
